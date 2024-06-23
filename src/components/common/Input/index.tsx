a7d4c1314b415dde17fa5b7b27ec8cb4a326fbab
KuharchukDmytro <kuharchuk.dmytro1337@gmail.com>
2024-06-23 23:00:19 +0300

    fix input styles with icon

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,6 +1,6 @@
 import { ChangeEventHandler, ComponentPropsWithRef, forwardRef } from 'react';
 import { FieldErrors, UseFormRegisterReturn } from 'react-hook-form';
-import { FlexContainer, If } from '..';
+import { FlexContainer, If, SvgIcon } from '..';
 
 import classnames from 'classnames';
 import styles from './index.module.scss';
@@ -9,22 +9,31 @@ type YupRegistration = Omit<Partial<UseFormRegisterReturn<string>>, 'onChange'>;
 
 type Props = YupRegistration & {
   onChange: ChangeEventHandler<HTMLInputElement>;
+  icon?: string;
+  iconSize?: number;
   label?: string;
   errors?: FieldErrors;
 } & ComponentPropsWithRef<'input'>;
 
 export const Input = forwardRef<HTMLInputElement, Props>(
-  ({ label, errors, className, ...props }, ref) => {
-    const id = props.id ?? Math.random().toString(36).slice(2, 15);
+  ({ label, errors, icon, iconSize = 24, className, ...props }, ref) => {
+    const id = props.id || Math.random().toString(36).slice(2, 15);
     const error = errors?.[props.name ?? '']?.message;
 
     return (
       <FlexContainer
-        direction='column'
+        direction='row'
+        align='center'
         gap={8}
-        className={styles.input_wrapper}
+        className={classnames(styles.input_wrapper, className, {
+          [styles.input_wrapper__with_icon]: icon,
+        })}
         data-error={error}
       >
+        <If condition={icon}>
+          <SvgIcon src={icon} className={styles.icon} size={iconSize} />
+        </If>
+
         <If condition={label}>
           <label htmlFor={id} className={styles.label}>
             {label}
@@ -34,7 +43,9 @@ export const Input = forwardRef<HTMLInputElement, Props>(
         <input
           type='text'
           id={id}
-          className={classnames(styles.input, className)}
+          className={classnames(styles.input, {
+            [styles.input__with_icon]: icon,
+          })}
           ref={ref}
           {...props}
         />
